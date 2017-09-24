b21f360686c1fe2a9d4119f91581e036c2a02161
protco <zxl1000@gmail.com>
2017-09-24 12:46:02 -0400

    update ui templates

--- jokes/urls.py
+++ jokes/urls.py
@@ -7,7 +7,7 @@ from .views import JokeCreateView, JokeUpdateView, JokeDeleteView, JokeUserView
 app_name = 'jokes'
 
 urlpatterns = [
-    url(r'^jokecreate/', JokeCreateView.as_view(), name='jokecreate'),
+    url(r'^jokecreate', JokeCreateView.as_view(), name='jokecreate'),
     url(r'^(?P<pk>\d+)/jokeupdate/$', JokeUpdateView.as_view(), name='jokeupdate'),
     url(r'^(?P<pk>\d+)/jokedelete/$', JokeDeleteView.as_view(), name='jokedelete'),
     url(r'^jokeusers/', JokeUserView.as_view(), name='jokeusers'),
--- userprofiles/urls.py
+++ userprofiles/urls.py
@@ -4,5 +4,5 @@ from userprofiles.views import ProfileHomeView, public_profile
 app_name = 'userprofiles'
 urlpatterns = [
     url(r'^$', ProfileHomeView.as_view(), name='profile-home'),
-    url(r'publicprofile/(?P<username>[a-zA-Z0-9]+)/$', public_profile, name='public_profile'),
+    url(r'publicprofile/(?P<username>[a-zA-Z0-9]+)$', public_profile, name='public_profile'),
 ]
